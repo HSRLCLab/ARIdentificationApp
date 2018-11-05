e26eae3cfb5ab4e53acc8e71dfb50cfcbb4cee15
fbud <fbuergle@hsr.ch>
2018-11-05 09:52:11 +0100

    First iteration finished
    Model will be shown statically.
    Buttons "Virtual Model" and "Paint" do something ( Virtual Model toggles the virtual model on and off, paint paints the whole model blue)

---
+++ ARIdentificationApp/Assets/Scripts/VirtualModelLoad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualModelLoad : MonoBehaviour {
+    public GameObject virtualModel;
+    public Material cyan;
+    public Material blue;
+    public Material magenta;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start called");
+        //paintVirtualModel();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Paint the virtual model (called from buttonclick)
+    public void PaintVirtualModel() {
+
+        //On load go through all children and set a material and a mesh collider
+        foreach (MeshRenderer component in virtualModel.GetComponentsInChildren<MeshRenderer>())
+        {
+            //Debug.Log("Found a child " + component.gameObject.name);
+            Debug.Log("Found a child with meshrenderer" + component.gameObject.name);
+            component.gameObject.AddComponent<MeshCollider>();
+            component.material = blue;
+        }
+    }
+}
