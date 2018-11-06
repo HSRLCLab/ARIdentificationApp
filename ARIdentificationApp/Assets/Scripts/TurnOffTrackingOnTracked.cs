60a7a58d522824e7e6edf4d225f5b51912cd3b9a
fbud <fbuergle@hsr.ch>
2018-11-06 16:55:38 +0100

    Started iteration2
    Added Startupcanvas (but disabled it as it would be annoying)
    Fixed bug that App-menue wasn't really white but more greyish -> is now totally white with red border
    Startet with iteration 2: positioning (made virtual model child of mode target, turned model target on, changed device tracking options in VuforiaConfigurator)
    Made a few test and hopefully undo anything not used.
    Changed VirtualModel Button so that it disables the Mesh Renderer instead of the whole object (making it invisible but still interactable)

---
+++ ARIdentificationApp/Assets/Scripts/TurnOffTrackingOnTracked.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+
+public class TurnOffTrackingOnTracked : DefaultTrackableEventHandler
+{
+    public Text Debug;
+    public GameObject virtualModel;
+
+
+    /*
+    void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+        TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+        Debug.text = " ModelTarget trackerStopped ";
+    }
+    */
+    protected override void OnTrackingLost()
+    {
+        //TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+        Debug.text += " ModelTarget Tracking lost ";
+    }
+
+    //Stop tracking if the object was tracked
+    protected override void OnTrackingFound() {
+        Debug.text = " ModelTarget Tracking found ";
+        //TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+
+        //virtualModel.transform.parent = newParent.transform;
+               
+    }
+}
--- ARIdentificationApp/Assets/Scripts/VirtualModelLoad.cs
+++ ARIdentificationApp/Assets/Scripts/VirtualModelLoad.cs
@@ -1,35 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class VirtualModelLoad : MonoBehaviour {
+public class VirtualModelLoad : MonoBehaviour
+{
     public GameObject virtualModel;
     public Material cyan;
     public Material blue;
     public Material magenta;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         Debug.Log("Start called");
         //paintVirtualModel();
 
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     //Paint the virtual model (called from buttonclick)
-    public void PaintVirtualModel() {
+    public void PaintVirtualModel()
+    {
 
-        //On load go through all children and set a material and a mesh collider
+        //Go through all children and set a material if they have a mesh collider
         foreach (MeshRenderer component in virtualModel.GetComponentsInChildren<MeshRenderer>())
         {
             //Debug.Log("Found a child " + component.gameObject.name);
-            Debug.Log("Found a child with meshrenderer" + component.gameObject.name);
+            Debug.Log("Found a child with meshrenderer: " + component.gameObject.name);
             component.gameObject.AddComponent<MeshCollider>();
-            component.material = blue;
+            Debug.Log("Found childs parent " + component.transform.parent.gameObject.name);
+            var parent = component.transform.parent.gameObject.name;
+            Material mymaterial;
+            int numericparent = int.Parse(parent);
+            if (numericparent % 3 == 0)
+            {
+                mymaterial = cyan;
+            }
+            else if (numericparent % 3 == 1)
+            {
+                mymaterial = magenta;
+            }
+            else
+            {
+                mymaterial = blue;
+            }
+
+            component.material = mymaterial;
+        }
+    }
+
+
+
+    public void HideVirtualModel(bool enable)
+    {
+        //Go through all children wîth mesh collider and disable the mesh collider (make it invisible but still interactable)
+        foreach (MeshRenderer component in virtualModel.GetComponentsInChildren<MeshRenderer>())
+        {
+            component.enabled = enable;
         }
     }
 }
