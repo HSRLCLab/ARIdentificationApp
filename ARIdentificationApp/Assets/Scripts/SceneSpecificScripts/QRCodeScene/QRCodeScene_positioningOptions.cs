5c38ccedef7494ee4c67040e3349697a247a34d5
fbud <fbuergle@hsr.ch>
2018-12-04 09:31:06 +0100

    Bug fixed in QR Code scene
    Fixed the wrong Reset Button in the positioning options of the QR Code scene. Added a new script (QRCodeScene_positioningOptions.) for that, which only contains the changed Reset() function.

---
+++ ARIdentificationApp/Assets/Scripts/SceneSpecificScripts/QRCodeScene/QRCodeScene_positioningOptions.cs
@@ -0,0 +1,40 @@
+﻿/* -----------------------------------------------------------------------
+	Date: 04.12.2018
+	Comment: Position the virtual model manually. Copied from positionngOptions.cs.
+            positionOptions.cs is still needed, as only the changed function is added here.
+	Author: Franziska Bürgler
+	
+---------------------------------------------------------------------------*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QRCodeScene_positioningOptions : MonoBehaviour {
+	public GameObject VirtualModel;
+	public GameObject ModelTarget;
+
+    private Vector3 initialPosition;
+    private Vector3 initialAngle;
+    private Vector3 initialPosDiff;
+    private Vector3 initialAngleDiff;
+	// Use this for initialization
+	void Start () {
+        initialPosDiff = VirtualModel.transform.position - ModelTarget.transform.position;
+        initialAngleDiff = VirtualModel.transform.eulerAngles - ModelTarget.transform.eulerAngles;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void Reset(){
+        //Reset the VirtualModel to it's initial position, or wherever the ImageTarget currently is
+        VirtualModel.transform.eulerAngles = ModelTarget.transform.eulerAngles + initialAngleDiff;
+        VirtualModel.transform.position = ModelTarget.transform.position + initialPosDiff;
+        
+    }
+}
